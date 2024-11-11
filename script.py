fb2eb50b8bd7c60c6949376f7113bac22b9ebb08
KarlKiudma <kiudmakarlmarkus@gmail.com>
2024-11-11 09:22:45 +0200

    Create script.py

---
+++ script.py
@@ -0,0 +1,7 @@
+# Kood pärineb Typed.js teegist, autor: Matt Boldt
+# Allikas: https://mattboldt.com/demos/typed-js/
+
+var typed = new Typed('.element', {
+    strings: ["First sentence.", "Second sentence."],
+    typeSpeed: 30
+});

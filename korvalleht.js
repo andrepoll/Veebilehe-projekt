cd019f099365be11615307b8a8d8c9c92965a20c
KarlKiudma <kiudmakarlmarkus@gmail.com>
2024-11-22 11:41:07 +0200

    Update korvalleht.js

--- korvalleht.js
+++ korvalleht.js
@@ -1,9 +1,8 @@
-
 // Tehtuks märkimine koostiosade ja valmistamisetappide jaoks
+
 document.querySelectorAll("ul li, ol li").forEach(item => {
     item.addEventListener("click", () => {
         item.classList.toggle("completed");
         // Klõpsamise tagasiside
-        item.style.backgroundColor = item.classList.contains("completed") ? "#d3ffd3" : "";
     });
 });

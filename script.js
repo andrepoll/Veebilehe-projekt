9ca784fe090fdfdab77e1ed2eea8b8df0890dcbf
andrepoll <andrepoll@LIVE.COM>
2024-11-22 12:53:06 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Kood pärineb Typed.js teegist, autor: Matt Boldt 
-// Allikas: https://mattboldt.com/demos/typed-js/
-
-var typed = new Typed('.element', {
-    strings: ["Tere tulemast retseptilehele!", "Leia oma lemmikretseptid siit!"],
-    typeSpeed: 50,
-    backSpeed: 50,
-    loop: true
-});
